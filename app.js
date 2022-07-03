57f3255513e7be16c6b4a9c23cacc020e9ada1a4
Ossai Lucky <luckyblaqy@gmail.com>
2022-07-03 15:56:07 +0100

    added a failure and sucesss page

--- app.js
+++ app.js
@@ -38,14 +38,22 @@ app.post("/", (req,res)=>{
 
     const jsonData = JSON.stringify(data)
 
-    const url = "url list id"
+    const url = "https://us13.api.mailchimp.com/3.0/lists/ADD YOUR LIST ID"
 
     const options ={
         method: "POST",
         auth: "ossai1:apikey"
     }
   const request =  https.request(url,options,(response)=>{
-        response.on("data", (data)=>{
+       
+    if(response.statusCode === 200){
+        res.sendFile(path.join(__dirname, "public/success.html"))
+    }else{
+        res.sendFile(path.join(__dirname, "public/failure.html"))
+    }
+    
+    
+    response.on("data", (data)=>{
             console.log(JSON.parse(data))
         })
     })
@@ -53,6 +61,10 @@ app.post("/", (req,res)=>{
     request.write(jsonData)
     request.end();
 
+})
+
+app.post("/public/failure.html",(req,res)=>{
+    res.redirect("/")
 })
 
 app.listen(3000,()=>{
